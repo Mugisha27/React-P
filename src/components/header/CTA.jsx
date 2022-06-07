2d8e8db34921baeb54d2055131761d9cbff07aa4
chris Mugisha <chrismugisha@chriss-MacBook-Air.local>
2022-06-06 21:05:46 -0700

    changes

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from '../../assets/cv.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-     <a href={CV} download>MY Resume</a>
+     <a href={CV} download>My Resume</a>
      <a href="#contact">Contact me Here</a>
     </div>
   )
